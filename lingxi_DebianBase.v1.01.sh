456e4b74065c72791ccef9830c2364d5d7b62085
zlingxi <bluefarcm@gmail.com>
2022-09-29 03:10:29 +0800

    update

--- lingxi_DebianBase.v1.01.sh
+++ lingxi_DebianBase.v1.01.sh
@@ -1,32 +1,4 @@
 #！/bin/bash
-
-//.profile change ------------------------------------------------------------------------------------------------------------------------
-echo 'export PATH=/usr/loca/sbin:/usr/sbin:/sbin:$PATH:' >> /root/.profile
-source /root/.profile
-echo 'export PATH=/usr/loca/sbin:/usr/sbin:/sbin:$PATH:' >> /home/lingxi/.profile && source  /home/lingxi/.profile
-
-//.bashrc change -------------------------------------------------------------------------------------------------------------------------
-echo "#root.bashrc conf 
-  alias ls='ls --color'
-  alias ls='ls $LS_OPTIONS'
-  alias ll='ls $LS_OPTIONS -l'
-  alias l='ls $LS_OPTIONS -lA'
-  alias rm='rm -i'
-  alias cp='cp -i'
-  alias mv='mv -i'" >> /root/.bashrc
-
-
-
-  echo "#lingxi.bashrc conf 
-  alias ls='ls --color'
-  alias ls='ls $LS_OPTIONS'
-  alias ll='ls $LS_OPTIONS -l'
-  alias l='ls $LS_OPTIONS -lA'
-  alias rm='rm -i'
-  alias cp='cp -i'
-  alias mv='mv -i'" >> /home/lingxi/.bashrc
-
-
 //change image sources.list --------------------------------------------------------------------------------------------------------------
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 touch /etc/apt/sources.list
@@ -43,13 +15,43 @@ echo "
     deb-src https://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib" >> /etc/apt/sources.list
     apt update -y && apt install apt upgrade -y 
 
+
+
+//.profile change ------------------------------------------------------------------------------------------------------------------------
+echo 'export PATH=/usr/loca/sbin:/usr/sbin:/sbin:$PATH:' >> /root/.profile
+source /root/.profile
+echo 'export PATH=/usr/loca/sbin:/usr/sbin:/sbin:$PATH:' >> /home/lingxi/.profile && source  /home/lingxi/.profile
+
+
+
+//.bashrc change -------------------------------------------------------------------------------------------------------------------------
+echo "#root.bashrc conf 
+  alias ls='ls --color'
+  alias ls='ls $LS_OPTIONS'
+  alias ll='ls $LS_OPTIONS -l'
+  alias l='ls $LS_OPTIONS -lA'
+  alias rm='rm -i'
+  alias cp='cp -i'
+  alias mv='mv -i'" >> /root/.bashrc
+
+  echo "#lingxi.bashrc conf 
+  alias ls='ls --color'
+  alias ls='ls $LS_OPTIONS'
+  alias ll='ls $LS_OPTIONS -l'
+  alias l='ls $LS_OPTIONS -lA'
+  alias rm='rm -i'
+  alias cp='cp -i'
+  alias mv='mv -i'" >> /home/lingxi/.bashrc
+
+
 source /root/.bashrc && source /home/lingxi/.bashrc
+
+
 
 //change ssh_config ----------------------------------------------------------------------------------------------------------------------
 echo "
     PermitRootLogin yes
     PasswordAuthentication yes" >> /etc/ssh/sshd_config && /etc/init.d/ssh restart
-
 
 
 //install package -------------------------------------------------------------------------------------------------------------------------
